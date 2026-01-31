a94efd1dc49ca55ebc9e06705109ffd804eedbc7
chesnakov2002 <dedpolikov@gmail.com>
2026-01-31 16:02:49 +0700

    Initial commit

--- Range/src/ru/academits/chesnakov/range/Range.java
+++ Range/src/ru/academits/chesnakov/range/Range.java
@@ -1,7 +1,4 @@
 package ru.academits.chesnakov.range;
 
 public class Range {
-  public static Main (String [] args){
-    
-  }
 }
--- Range/src/ru/academits/chesnakov/rangeMain/Main.java
+++ Range/src/ru/academits/chesnakov/rangeMain/Main.java
@@ -2,6 +2,6 @@ package ru.academits.chesnakov.rangeMain;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(1234);
     }
 }

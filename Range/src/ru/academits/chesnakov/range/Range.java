4925f3d5245d2eb68f17526acef5c82dcd4ecff1
chesnakov2002 <dedpolikov@gmail.com>
2026-02-02 17:02:19 +0700

    Range

--- Range/src/ru/academits/chesnakov/range/Range.java
+++ Range/src/ru/academits/chesnakov/range/Range.java
@@ -1,4 +1,84 @@
 package ru.academits.chesnakov.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    @Override
+    public String toString() {
+        return "Range{" +
+                "from = " + from +
+                ", to = " + to +
+                '}';
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public double getRangeLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        if (number >= from) {
+            return number <= to;
+        }
+
+        return false;
+    }
+
+    public Range getIntersection(Range range) {
+        if (this.getTo() <= range.getFrom() || this.getFrom() >= range.getTo()) {
+            return null;
+        }
+
+        double maxFrom = Math.max(this.getFrom(), range.getFrom());
+        double minTo = Math.min(this.getTo(), range.getTo());
+
+        return new Range(maxFrom, minTo);
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.getTo() < range.getFrom() || this.getFrom() > range.getTo()) {
+            return new Range[]{new Range(this.getFrom(), this.getTo()),
+                    new Range(range.getFrom(), range.getTo())};
+        }
+
+        double minFrom = Math.min(this.getFrom(), range.getFrom());
+        double maxTo = Math.max(this.getTo(), range.getTo());
+
+        return new Range[]{new Range(minFrom, maxTo)};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (this.getFrom() == range.getFrom() && this.getTo() == range.getTo()) {
+            return new Range[]{};
+        }
+
+        if (this.getFrom() >= range.getTo() || this.getTo() <= range.getFrom()) {
+            return new Range[]{new Range(this.getFrom(), this.getTo()),
+                    new Range(range.getFrom(), range.getTo())};
+        }
+
+        return new Range[]{new Range(this.getFrom(), this.getTo()),
+                new Range(range.getFrom(), range.getTo())};
+    }
 }
--- Range/src/ru/academits/chesnakov/rangeMain/Main.java
+++ Range/src/ru/academits/chesnakov/rangeMain/Main.java
@@ -1,7 +1,38 @@
 package ru.academits.chesnakov.rangeMain;
 
+import ru.academits.chesnakov.range.Range;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1234);
+        Range range1 = new Range(1.1, 3.3);
+        Range range2 = new Range(1, 6.5);
+
+        range1.setFrom(1);
+        range1.setTo(8);
+
+        System.out.println(range1.getFrom());
+        System.out.println(range1.getTo());
+
+        System.out.println(range1.getRangeLength());
+
+        System.out.println(range1.isInside(1));
+        System.out.println(range2.isInside(5));
+
+        Range range3 = range1.getIntersection(range2);
+
+        System.out.println(range3.getFrom());
+        System.out.println(range3.getTo());
+
+        Range range4 = new Range(11, 33);
+
+        Range[] rangesArray1 = range1.getUnion(range4);
+        System.out.println(Arrays.toString(rangesArray1));
+
+        Range range5 = new Range(11, 22);
+
+        Range[] rangesArray2 = range4.getDifference(range5);
+        System.out.println(Arrays.toString(rangesArray2));
     }
 }
